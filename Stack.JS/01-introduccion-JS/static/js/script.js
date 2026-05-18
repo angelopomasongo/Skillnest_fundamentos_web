da78957406683ed4bee0280e25b696c951acf2c8
angelopomasongo <angelopomasongo@liceovvh.cl>
2026-05-18 14:35:46 -0400

    nuevo

---
+++ Stack.JS/01-introduccion-JS/static/js/script.js
@@ -0,0 +1,9 @@
+console.log("Conexion exitosa con js...")
+function cambiarTexto() {
+  let titulo = "Hola, bienvenido";
+  if (document.getElementById("titulo").innerText == titulo){
+      document.getElementById("titulo").innerText = "Has cambiado el texto con JS";
+  } else {
+    document.getElementById("titulo").innerText = titulo;
+  }
+}
