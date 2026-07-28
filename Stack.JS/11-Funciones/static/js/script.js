6862f8dfa3b019e44edee8f32609870b8049b600
angelopomasongo <angelopomasongo@liceovvh.cl>
2026-07-28 11:14:33 -0400

    nuevo

---
+++ Stack.JS/11-Funciones/static/js/script.js
@@ -0,0 +1,25 @@
+console.log("Conexion exitosa...");
+
+//ejemplo funcion simple (sin parametros)
+function saludar(/*parametros*/) {
+   alert("¡Hola, bienvenido!");
+}
+//saludar();//ejecucion de una funcion
+
+// function saludar(nombre) {
+//    alert("¡Hola, " + nombre + "!");
+// }
+// saludar("Luis");
+// saludar("Ana");
+
+// function encontrarMaximo(a, b) {
+//    if (a > b) {
+//        return a;
+//    } else {
+//        return b;
+//    }
+// }
+// let numero1 = 10;
+// let numero2 = 7;
+// let maximo = encontrarMaximo(numero1, numero2);
+// console.log("El número mayor entre", numero1, "y", numero2, "es:", maximo);
