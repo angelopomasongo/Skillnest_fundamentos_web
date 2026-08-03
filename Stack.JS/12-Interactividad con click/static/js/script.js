0a5d1faee32faf2ac3287fc1b7083cdfe2644841
angelopomasongo <angelopomasongo@liceovvh.cl>
2026-08-03 14:43:04 -0400

    nuevo

--- Stack.JS/12-Interactividad con click/static/js/script.js
+++ Stack.JS/12-Interactividad con click/static/js/script.js
@@ -1,11 +1,11 @@
-console.log("Conexion exitosa...");
+console.log("Prueba de conexion Js...");
 
 document.getElementById("colorButton").addEventListener("click", cambiarColor);
-document.getElementById("colorButton").addEventListener("click", cambiarColor);
-document.getElementById("colorButton").addEventListener("click", cambiarColor);
+document.getElementById("colorButton1").addEventListener("click", cambiarColor);
+document.getElementById("colorButton2").addEventListener("click", cambiarColor);
 
 function cambiarColor() {
-    let colores = ["#ff6347", "#66cdaa", "#9370db", "#ffd700", "#4682b4", "#ffa07a"];
+    let colores = ["#ff6347", "#66cdaa", "#9370db", "#534c24", "#4682b4", "#ffa07a"];
     let colorAleatorio = colores[Math.floor(Math.random() * colores.length)];
     document.body.style.backgroundColor = colorAleatorio;
 }
